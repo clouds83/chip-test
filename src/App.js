7cf0db0d98eb008cc90d3a7f66a4e189e17fea2c
Claudio Godoy <clouds83@gmail.com>
2024-01-30 23:30:07 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+const stores = [
+  '93384',
+  '3948',
+  '87346',
+  '23834',
+  '37345',
+  '3945',
+  '02393',
+  '3948',
+  '76534',
+  '48356',
+  '2039',
+  '29338',
+  '1029',
+  '20339',
+  '4920',
+  '10329',
+]
+
+const amountOfStoresToShow = 10
+
+const storesToRender = stores.slice(0, amountOfStoresToShow)
+
+const remainingStores = stores.slice(amountOfStoresToShow)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='left'></div>
+      <div className='right'>
+        <div className='storesContainer'>
+          {storesToRender.map((store, index) => (
+            <div key={index} className='store'>
+              {store}
+            </div>
+          ))}
+          <div className='remaining'>+{remainingStores.length} stores</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
